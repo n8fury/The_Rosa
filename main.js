7a568d5baf9f051461c30c0e761744987282c59a
Mamdud Hasan <mhjoy547@yahoo.com>
2022-04-27 23:53:54 +0600

    modified element selection

--- main.js
+++ main.js
@@ -1,11 +1,5 @@
-//element function
-
-const selectElement = function (element) {
-  return document.querySelector(element);
-};
-
-let menuToggler = selectElement(".menu-toggle");
-let body = selectElement("body");
+let menuToggler = document.querySelector(".menu-toggle");
+let body = document.querySelector("body");
 
 menuToggler.addEventListener("click", function () {
   body.classList.toggle("open");
